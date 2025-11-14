463ec36ca54912a280abd77154a16ccef7778386
Achuthankrishna <74654704+Achuthankrishna@users.noreply.github.com>
2025-11-14 03:12:05 -0800

    PegSlot Env + Agent (#1)
    * Adding robosuite repo
    
    * Creating XML and with the objects
    
    * Missing torch installation and fmpeg
    
    * Naive cube place env implementation based off lift.py
    
    * Used SequentialSampler for 2 objects like nut_assemgly.py
    
    * removed random sampling and using constant positioning
    
    * changed success condition and teleop saving format for h5
    
    * Performing convex decomposition of the mesh for detecting concavity
    
    * saving episode issues + PandaRobot
    
    * agent creation
    
    * Added subtask 2 + sim loop
    
    * Pushed down env cameras
    
    * Added subtask3 and 4
    
    * Subtask 5 and playback
    
    * subtask 5 + refinements (heurestics)
    
    * polish +json
    
    * polish +json2
    
    * Added data + teleop videos
    
    * ReadME updates
    
    * readmeupdate2
    
    * readmeupdate3
    
    * readmeupdate4
    
    * readmeupdate5
    
    * readmeupdate6
    
    * readmeupdat76
    
    * readmeupdat8

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # PegSlotSkill
-Repository for simulating complex robot tasks for synthetic dataset and heurestic based task completion.
+This repo is a supporting simulation software for peg slot skill which are used for data generation, testing and eval of robot skills learning. The current simulation architecture adopts Robosuite which can be supported by ,odular architectures like mimicgen for data generation purposes and robomimic for RL training. The repo also consists of an heurestic agent that can simulate the action purely based on adjustments and control.
+
+## Installation steps:
+The installation heirarchy will follow from creating a new conda env and installing all required repos induvidually:
+1. Set up conda environment:l
+```sh
+conda create -c conda-forge -n robot-skills-sim python=3.10
+```
+2. Activate conda environment:
+```sh
+conda activate robot-skills-sim
+```
+3. Clone and setup robosuite dependency:
+
+```sh
+cd robosuite
+pip install -e .
+pip install -r requirements.txt
+pip install -r requirements-extra.txt
+```
+(optional: if running into issues with numba/numpy, run: conda install -c numba numba=0.56.4 -y)
+
+4. Re-Install Numpy and Numba versions (If faced with error):
+```sh
+pip install numpy==1.23.3 numba==0.56.4
+```
+## Custom Env Creation
+If users want to create a custom env for separate tasks , follow the instructions below :
+1. Add object stls into `robosuite/models/assets/objects/meshes` folder
+2. Create respective object xml by following sample xml templates on the `robosuite/models/assets/objects` folder
+3. Create Object classes inside `robosuite/robosuite/models/objects/xml_objects.py` referencing the xml file.
+4. Call the Object class anywhere in the code to initialize it
+
+**Note : All custom assets are convex hulls when loaded. If you want to ensure concavity perform convex decomposition**
+
+### Convex Decomposition
+Users can perform convex decomposition on convex hull stls using the following code
+```sh
+python robosuite/robosuite/models/assets/objects/convex_decomposition.py --mesh_path XXX/robosuite/robosuite/models/assets/objects/meshes/cuboid.stl
+```
+This will split into n meshes , where n can be passed as args. All these meshes can be used to create a new object xml.
+
+## Teleoperation Skill
+Users can teleop the robot + gripper using the following scripts which saves the recording as Hdf5 file consisting of states and observations required for RL and also mimicgen. The base saving format is robomimic.
+
+Before running teleop ,users need to compile any new task that's being aded to set of registered task . This can be done using:
+```sh
+ python -m py_compile cube_place.py 
+```
+Now,we can run teleop on CubePlace Task
+```sh
+python robosuite/scripts/collect_human_demonstrations.py --device keyboard --environment CubePlace --robots PandaRobotiq --directory XXXX/PegSlotSkill/robosuite_data_2"
+```
+where XXX can be the directory user wants to save the data in. Users can choose from a list of tasks .
+
+The teleop-ed data can be played back using a playback script borrowed from Robocasa. 
+```sh
+python robosuite/scripts/playback_dataset.py --dataset XXXX/robosuite_data_2/demo_2025-11-XXXX.hdf5  --use-actions --n 1
+```
+here `use-actions` is used to step the ennvironments with the actions stored in the H5 file. 
+
+A sample teleop playback using actions
+
+![Video](./robosuite_data_2/use_action.gif)
+
+## Agent Simulation
+We have created a simulated agent that's purely hurestics based. The task is subdivided into n subtasks for easier control and execution of this heurestics based controller. The final success condition checks for contact between peg_geom and slot_geom_0.
+
+The created agent can be run using the following script:
+```sh
+python robosuite/scripts/playback_agent.py --log_path robosuite/sim_log.json
+```
+where users can control the following args : 
+* `sim_speed` which controls the simulation update rate (default 0.002 in Mujoco)
+* `step_speed` which controls the speed at which the simulation step loop runs in . Deault it has 0.1 delay,but can be made faster by passing a num value
+* `episode_length` which controls the length of the entire episode ie; traj len
+
+The following code opens a mujoco window and executes the agent controlled by OSC controller with every subtask mentioning the end effector goal pos. The file outputs `sim_log.json` which covers the following modalities:
+* `commanded_robot_pos` : A 7D vector which has commanded robot poses [x,y,z,ax,ay,az,gripper_val]
+* `gripper_pose_observed`: A 7D vector which has observed robot poses obtained from stepping the environment using the commanded values in [x,y,z,ax,ay,az,gripper_val]
+* `peg_pos_commanded` : The commanded peg_pos obtained when env.reset() is called
+* `hole_pos_commanded` : The commanded peg_pos obtained when env.reset() is called
+* `success` : Whether the episode reached success conditions
+
+The following videos were obtained when played in :
+1. Deault `sim_speed` and `step_speed`
+![video2](./robosuite_data_2/place_0.002.gif)
+
+2. `step_speed` at 10 and default `sim_speed`
+
+![video3](./robosuite_data_2/playat10x.gif)
+
+3. `sim_speed` at 0.005 and default `step_speed`
+![video4](./robosuite_data_2/largersimupdate0.005.gif)
+
+
---
+++ robosuite/robosuite/scripts/playback_agent.py
@@ -0,0 +1,248 @@
+#creating dummy agent via huerestics to simulate
+import numpy as np
+import robosuite as suite
+import time
+import json
+import os 
+from robosuite.utils.sim_utils import check_contact
+import robosuite.macros as macros
+import argparse
+class PickPlaceAgent:
+    """
+    Scripted agent for peg-in-hole test with subtasks:
+    1. Reach peg and close gripper
+    2. Lift peg
+    3. rotate arm to match slot config
+    4. bring arm down closer to hole pose
+    5. release gripper
+    """
+    def __init__(self,lift_height=0.15, jaw_open=0.0, jaw_close=0.5):
+        self.step=0
+        self.reach_pos=None
+        self.lift_height = lift_height
+        self.jaw_open = jaw_open
+        self.jaw_close = jaw_close
+        self.reach2_pos=None
+        self.subtask = 1
+    
+    def perform_action(self,state,env=None):
+        """
+        Performs actions via 5 subtask to place cube on peg given fixed location
+        Args:
+            state: Simulation State
+            env :Sim Environment Variable. Defaults to None.
+        """
+        success = False
+        done = False
+        if self.reach_pos is None:
+            if env is not None:
+                #location of cube and cuboid slot
+                self.reach_pos=env.sim.data.get_site_xpos("peg_default_site").copy() 
+                self.reach2_pos = env.sim.data.get_site_xpos("slot_default_site").copy()
+        #using right griopper as site since its one that closes
+        gripper_pos = env.sim.data.get_site_xpos("gripper0_right_grip_site")
+
+        # print(self.reach_pos,gripper_pos)
+
+        if self.subtask == 1:
+            target_pos = [self.reach_pos[0]+0.01,self.reach_pos[1], self.reach_pos[2]-0.01]
+            jaw = self.jaw_open
+            # Close jaw when near peg
+            print("Norm dist between gripper and slot",np.linalg.norm(gripper_pos - target_pos))
+            if np.allclose(gripper_pos, target_pos, atol=1e-2):
+                # breakpoint()
+                print("Closing gripper on peg")
+                jaw = self.jaw_close
+                if check_contact(env.sim,env.robots[0].gripper['right'].contact_geoms[10],env.objects[0].contact_geoms[0]):
+                    self.subtask = 2
+            pos = 3.0*(target_pos - gripper_pos)
+            print("pos:", pos)
+            ori = np.zeros(3)
+        #subtask2 : lift the  peg
+        elif self.subtask==2:
+            print("lifting Peg")
+            #target as height
+            target_pos = np.array([self.reach_pos[0],self.reach_pos[1],self.lift_height])
+            jaw=self.jaw_close
+            print("target_pos:", target_pos)
+            print("gripper_pos:", gripper_pos)
+            if np.allclose(gripper_pos, target_pos, atol=1e-2):
+                print("Reached lift height, holding position")
+                self.subtask=3
+            #update pos
+            pos=1.5*(target_pos-gripper_pos)
+            #flip the eef by 90 deg by roll
+            target_ori=[np.pi/4,0,0]
+            ori=0.1*(np.array(target_ori)-np.zeros(3))
+        #Subtask 3: Go near the slot
+        elif self.subtask==3:
+            print("Moving peg to slot")
+            #rech2pos as target + some hegith threshold
+            target_pos=[self.reach2_pos[0],self.reach2_pos[1],self.reach2_pos[2]+self.lift_height]
+            jaw=self.jaw_close
+            print("L2 distance for task 3",np.linalg.norm(gripper_pos-target_pos))
+            #threshold selection : closer to the target + threshold
+            if np.allclose(gripper_pos, target_pos, atol=1e-2):
+                self.subtask=4
+            pos=1.0*(target_pos-gripper_pos)
+            ori=np.zeros(3)
+
+        #subtask4: Rotate along pitch by 120
+        elif self.subtask==4:
+            print("Bringing closer to placing")
+            # target_pos = [self.reach2_pos[0]+0.025,self.reach2_pos[1]-0.03, self.reach2_pos[2]+0.1]
+            target_pos = [self.reach2_pos[0]+0.025,self.reach2_pos[1]-0.03, self.reach2_pos[2]+0.1]
+            jaw=self.jaw_close
+            if np.allclose(gripper_pos, target_pos, atol=1e-2):
+                print("Reached lift height, holding position")
+                #break the loop
+                self.subtask = 5  # Hold position
+            pos =1.5*(target_pos - gripper_pos)
+            target_ori=[0.0,2*np.pi/3,0.0]
+            #Calculate ori error with current ori of gripper
+            from scipy.spatial.transform import Rotation as R
+            current_rot_mat=env.sim.data.get_site_xmat("gripper0_right_grip_site").reshape(3,3)
+            # print(current_rot_mat)
+            #RotMat to Euler
+            current_euler_ori=R.from_matrix(current_rot_mat).as_euler('xyz')
+            orien_error=target_ori-current_euler_ori
+            #L2 ori error
+            ori_norm=np.linalg.norm(orien_error)
+            #Dont rotate much - so stop ori early
+            if ori_norm > 2.0:
+                #slow update
+                ori=0.05*orien_error
+            else:
+                ori=np.zeros(3)
+        #subtask 5: place into the slot
+        elif self.subtask==5:
+            print("Final Subtask")
+            target_pos = [self.reach2_pos[0]-0.02,self.reach2_pos[1]-0.018, self.reach2_pos[2]+0.025]
+            jaw=self.jaw_close
+            if np.allclose(gripper_pos, target_pos, atol=1e-2):
+                print("Placed on slot")
+                #break the loop
+                jaw = -1.0
+                done=True
+            pos = 3.0*(target_pos-gripper_pos) #pretty faster
+            target_ori = [0,0,0]
+            ori= 0.1*(np.array(target_ori) - np.zeros(3))
+            from scipy.spatial.transform import Rotation as R
+            current_rot_mat = env.sim.data.get_site_xmat("gripper0_right_grip_site").reshape(3,3)
+            current_ori = R.from_matrix(current_rot_mat).as_euler('xyz')
+            ori_error = target_ori - current_ori
+            ori_norm = np.linalg.norm(ori_error)
+            print("ori_error:", ori_norm)
+            # Stop rotating once near target
+            if ori_norm < 1.5:
+                ori = -0.05 * ori_error
+            else:
+                print("Reached target orientation — holding steady")
+                ori = np.zeros(3)
+
+        else:
+            pos = np.zeros(3)
+            ori = np.zeros(3)
+            jaw = -1.0
+        if env._check_success():
+            success=True
+
+
+
+        self.step += 1
+        return np.concatenate([pos, ori, [jaw]]),done,success
+
+def play_sim(agent,speed=1.0,episode_len=100,log_path="sim_log.json",control_freq=20):
+    """
+    Main Simulating function
+    Args:
+        agent : Agent Class
+        speed : Simulation speed. Defaults to 1.0.
+        episode_len : Length of the episode. Defaults to 100.
+        log_path : file name with path. Defaults to "sim_log.json".
+        control_freq: Frequency where IK updates are made . Deaults to 20
+    """
+    #make env
+
+    env=suite.make(
+        "CubePlace",
+        has_renderer=True,
+        has_offscreen_renderer=False,
+        use_camera_obs=False,
+        control_freq=control_freq,
+        robots="PandaRobotiq",
+
+    )
+    data=[]
+    #set state with current env observation (static state)
+    obs=env.reset()
+    #sim warmup : Good practice to not make the objects to fall
+    for _ in range(30):
+        env.sim.step()
+    time.sleep(0.1)
+    for s in range(episode_len):
+        print("step_number",s)
+        action,done,success=agent.perform_action(obs,env=env)
+        #so the action modality is 6 + 1 for jaw
+        pose=action[:6]
+        jaw=action[6]
+        #apply to env and step
+        obs,_,_,_=env.step(np.concatenate([pose,[jaw]]))
+        #to update per speed
+        sim_state = {
+            "step": int(s),
+            "gripper_pose_commanded": pose.tolist(),
+            "gripper_pose_observed": obs['robot0_joint_pos'].tolist(),
+            "jaw_cmd": float(jaw) if not isinstance(jaw, (list, np.ndarray)) else jaw.tolist(),
+            "peg_pos_commanded": env.sim.data.get_site_xpos("peg_default_site").tolist(),
+            "hole_pos_commanded": env.sim.data.get_site_xpos("slot_default_site").tolist(),
+            "gripper_to_cube_pos": obs['gripper_to_cube_pos'].tolist(),
+            "robot_eef_pos": obs['robot0_eef_pos'].tolist(),
+            "success":success,
+        }
+
+
+
+        time.sleep(0.01/speed)
+        #just to terminate eraly
+        if s>2500:
+            if done:
+                break
+        if success:
+            break
+        data.append(sim_state)
+    with open(log_path, "w") as f:
+        json.dump(data, f, indent=2)
+
+    env.close()
+    print("Done simulating")
+
+#TestCall
+if __name__ == "__main__":
+    agent=PickPlaceAgent()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--sim_speed",
+        type=float,
+        default=0.002,
+        help="Effective simulation time in seconds ; ie - how often env.step() updates"
+    )
+    parser.add_argument("--episode_length", 
+                        type=int, 
+                        default=5000,
+                        help="Entire episode length")
+    parser.add_argument(
+        "--log_path",
+        type=str,
+        default="./",
+        help="default log path",
+    )
+    parser.add_argument("--step_speed", 
+                        type=int, 
+                        default=1,
+                        help="Speed at which playback loop runs")
+    args = parser.parse_args()
+
+    macros.SIMULATION_TIMESTEP=args.sim_speed
+    play_sim(agent,episode_len=args.episode_length,speed=args.step_speed,log_path=args.log_path)
+    
